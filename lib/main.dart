19e7b6e053867dc999a3e7b234c60c00605af399
Ahmad Naji <ahmad.k.naji@gmail.com>
2020-10-08 01:37:03 +0300

    added image and icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,22 @@
 import 'package:flutter/material.dart';
 
+// The main function is starting point for all flutter apps
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Push-ups Counter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _resetCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _counter == 1
-                  ? 'You have done $_counter pushup'
-                  : 'You have done $_counter pushups',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline5,
-            ),
-
-            FloatingActionButton(
-              onPressed: _resetCounter,
-              tooltip: 'Reset',
-              child: Icon(Icons.refresh),
-              heroTag: null,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-
-          ],
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('عرصات members'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                AssetImage('images/IMG_20190217_101137_079.jpg')
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-        heroTag: null,
-      ),
-
-    );
-  }
+    ),
+  );
 }
